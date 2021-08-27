e56e5a3c62b6e36af32afe025ba89b28feb888dd
1g0rbm <m1g0rb89@gmail.com>
2021-08-27 19:17:00 +0300

    Added react to project

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => (
+  <div className="container-lg h-100 p-3">
+    <div className="h-100" id="chat">
+      <div className="card text-center">
+        <div className="card-body">
+          <h5 className="card-title">Project frontend l4 boilerplate</h5>
+          <p className="card-text">It works!</p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+// @ts-check
+
+import 'core-js/stable/index.js';
+import 'regenerator-runtime/runtime.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+import '../assets/application.scss';
+
+if (process.env.NODE_ENV !== 'production') {
+  localStorage.debug = 'chat:*';
+}
+
+ReactDOM.render(<App />, document.getElementById('chat'));
+
+console.log('it works!');
