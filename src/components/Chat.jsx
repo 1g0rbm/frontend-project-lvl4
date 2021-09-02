1470adb3ca2a21109ba31a8a6f1e3a9df2d570cc
1g0rbm <m1g0rb89@gmail.com>
2021-09-02 21:39:10 +0300

    Added data request in chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import authContext from '../context/authContext.jsx';
+import useHttp from '../hooks/useHttp.js';
 
-const Chat = () => (
-  <h2>Chat</h2>
-);
+const Chat = () => {
+  const auth = useContext(authContext);
+  const { request } = useHttp();
+
+  useEffect(async () => {
+    const data = await request(
+      '/api/v1/data',
+      'GET',
+      null,
+      {
+        Authorization: `Bearer ${auth.token}`,
+      },
+    );
+
+    console.log('DATA: ', data);
+  }, [request, auth]);
+
+  return (
+    <h2>Chat</h2>
+  );
+};
 
 export default Chat;
