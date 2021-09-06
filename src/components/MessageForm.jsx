aeab660c983beab2105fafe07a196496d421314b
1g0rbm <m1g0rb89@gmail.com>
2021-09-06 12:02:21 +0300

    Added useSocket hook

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,25 +1,37 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import { Field, Form as FormikForm, Formik } from 'formik';
 import Form from 'react-bootstrap/Form';
 import { useSelector } from 'react-redux';
-import SocketContext from '../context/socketContext.jsx';
 import AuthContext from '../context/authContext.jsx';
+import useSocket from '../hooks/useSocket.js';
 
 export default () => {
-  const { socket } = useContext(SocketContext);
   const { currentChannelId } = useSelector(({ channelsData }) => channelsData);
   const { username } = useContext(AuthContext);
+  const { emit } = useSocket();
+  const inputRef = useRef(null);
 
   const handleSubmit = ({ text }, bag) => {
-    bag.setSubmitting(false);
-    socket.emit(
+    bag.setSubmitting(true);
+    emit(
       'newMessage',
       { author: username, channelId: currentChannelId, text },
-      (response) => {
-        console.log('RESPONSE: ', response);
+      () => {
+        bag.setSubmitting(false);
+        bag.resetForm();
+        inputRef.current?.focus();
+      },
+      () => {
+        bag.setSubmitting(false);
+        bag.resetForm();
+        inputRef.current?.focus();
       },
     );
   };
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   return (
     <div className="mt-auto px-5 py-3">
@@ -27,20 +39,22 @@ export default () => {
         initialValues={{ text: '' }}
         onSubmit={handleSubmit}
       >
-        {({ isSubmitting, values }) => (
+        {({ isSubmitting, values, handleChange }) => (
           <FormikForm className="py-1 border rounded-2">
             <Form.Group className="input-group has-validation">
-              <Field
+              <Form.Control
+                onChange={handleChange}
                 type="text"
                 name="text"
                 placeholder="text"
                 value={values.text}
                 className="border-0 p-0 ps-2 form-control"
+                ref={inputRef}
               />
               <div className="input-group-append">
                 <button
                   type="submit"
-                  className="btn btn-outline-light btn-group-vertical"
+                  className="btn btn-outline-primary btn-group-vertical"
                   disabled={isSubmitting || !values.text}
                 >
                   Send
--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -2,7 +2,21 @@ import { useContext } from 'react';
 import SocketContext from '../context/socketContext.jsx';
 
 export default () => {
-  const socket = useContext(SocketContext);
+  const { socket } = useContext(SocketContext);
 
-  console.log('USE_SOCKET: ', socket);
+  const emit = (channel, message, resolve = () => {}, reject = () => {}) => {
+    socket.emit(
+      'newMessage',
+      message,
+      (response) => {
+        if (response.status === 'ok') {
+          resolve();
+        } else {
+          reject();
+        }
+      },
+    );
+  };
+
+  return { emit };
 };
