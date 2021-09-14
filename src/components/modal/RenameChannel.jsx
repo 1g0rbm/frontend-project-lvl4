31d8c6ace36f9acc5ebb176ba9ba38feaa61b15c
1g0rbm <m1g0rb89@gmail.com>
2021-09-15 01:22:18 +0300

    Added translation for modals

--- src/components/modal/RenameChannel.jsx
+++ src/components/modal/RenameChannel.jsx
@@ -1,14 +1,16 @@
 import React, { useRef, useEffect, useState } from 'react';
 import {
-  Alert, Button, Form, Modal,
+  Alert, Button, FloatingLabel, Form, Modal,
 } from 'react-bootstrap';
 import { Formik, Form as FormikForm } from 'formik';
 import { useSelector, useDispatch } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import cn from 'classnames';
 import validators from '../../validators.js';
 import useSocket from '../../hooks/useSocket.js';
 
 export default ({ hide, data }) => {
+  const { t } = useTranslation();
   const { channels } = useSelector(({ channelsData }) => channelsData);
   const inputRef = useRef(null);
   const { emit } = useSocket();
@@ -23,7 +25,7 @@ export default ({ hide, data }) => {
   return (
     <>
       <Modal.Header>
-        <h3>Rename channel</h3>
+        <h3>{t('text.renameChannel')}</h3>
       </Modal.Header>
       <Modal.Body>
         <Formik
@@ -40,7 +42,7 @@ export default ({ hide, data }) => {
               () => {
                 setSubmitting(false);
                 inputRef.current?.focus();
-                setError('Network error');
+                setError('error.network');
               },
             );
           }}
@@ -53,36 +55,37 @@ export default ({ hide, data }) => {
           }) => (
             <FormikForm>
               <Form.Group>
-                {formError && <Alert variant="danger">{formError}</Alert>}
+                {formError && <Alert variant="danger">{t(formError)}</Alert>}
               </Form.Group>
-              <Form.Group className="input-group has-validation mb-3">
-                <Form.Control
-                  onChange={handleChange}
-                  type="text"
-                  name="name"
-                  placeholder="Channel"
-                  ref={inputRef}
-                  value={values.name}
-                  className={cn({
-                    'form-control': true,
-                    'is-invalid': !!errors?.name,
-                  })}
-                />
-                <Form.Control.Feedback
-                  type="invalid"
-                  className={cn({ 'ps-3': true, 'd-block': !!errors?.name })}
-                >
-                  {errors.name}
-                </Form.Control.Feedback>
+              <Form.Group className="mb-3">
+                <FloatingLabel label={t('label.channel')}>
+                  <Form.Control
+                    onChange={handleChange}
+                    type="text"
+                    name="name"
+                    placeholder={t('label.channel')}
+                    ref={inputRef}
+                    value={values.name}
+                    className={cn({
+                      'form-control': true,
+                      'is-invalid': !!errors?.name,
+                    })}
+                  />
+                  <div className="invalid-tooltip">
+                    {t(errors.name)}
+                  </div>
+                </FloatingLabel>
               </Form.Group>
               <Form.Group className="d-flex justify-content-end">
-                <Button className="me-2" onClick={hide} variant="secondary">Cancel</Button>
+                <Button className="me-2" onClick={hide} variant="secondary">
+                  {t('button.cancel')}
+                </Button>
                 <Button
                   type="submit"
                   variant="primary"
                   disabled={isSubmitting || !values.name || !!errors?.name}
                 >
-                  Rename
+                  {t('button.rename')}
                 </Button>
               </Form.Group>
             </FormikForm>
