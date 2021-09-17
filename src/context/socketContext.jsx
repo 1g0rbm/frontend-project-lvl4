b627605b683dd12ea878ebb195aa7e2b91409fa7
1g0rbm <m1g0rb89@gmail.com>
2021-09-17 22:28:34 +0300

    Fixed socket initiation

--- src/context/socketContext.jsx
+++ src/context/socketContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useEffect } from 'react';
 import {
   newChannel, removeChannel, renameChannel, setCurrentChannelId,
 } from '../slices/channelsDataSlice.js';
@@ -10,33 +10,35 @@ const socketContext = createContext({ socket: null });
 const SocketContextProvider = ({ children, store, socket }) => {
   const { username } = useContext(authContext);
 
-  socket.on(
-    'newMessage',
-    (message) => {
-      store.dispatch(newMessage(message));
-    },
-  );
-  socket.on(
-    'newChannel',
-    (channel) => {
-      store.dispatch(newChannel(channel));
-      if (channel?.owner === username) {
-        store.dispatch(setCurrentChannelId(channel.id));
-      }
-    },
-  );
-  socket.on(
-    'removeChannel',
-    (message) => {
-      store.dispatch(removeChannel(message));
-    },
-  );
-  socket.on(
-    'renameChannel',
-    (message) => {
-      store.dispatch(renameChannel({ channel: message }));
-    },
-  );
+  useEffect(() => {
+    socket.on(
+      'newMessage',
+      (message) => {
+        store.dispatch(newMessage(message));
+      },
+    );
+    socket.on(
+      'newChannel',
+      (channel) => {
+        store.dispatch(newChannel(channel));
+        if (channel?.owner === username) {
+          store.dispatch(setCurrentChannelId(channel.id));
+        }
+      },
+    );
+    socket.on(
+      'removeChannel',
+      (message) => {
+        store.dispatch(removeChannel(message));
+      },
+    );
+    socket.on(
+      'renameChannel',
+      (message) => {
+        store.dispatch(renameChannel({ channel: message }));
+      },
+    );
+  }, [socket]);
 
   return (
     <socketContext.Provider value={{ socket }}>
